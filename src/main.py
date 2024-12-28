582c21f763bd2e0790c08c7580463a403f985171
Userkare <Tom_Helmut@yahoo.de>
2024-12-28 20:35:04 +0100

    coverthon to HTML

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from textnode import TextNode
 
+
 def main():
     text_nod = TextNode("This is a text node", "bold", "https://www.boot.dev")
     print(text_nod)
+
 
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,15 +1,19 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+class TextType(Enum):    
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
 
 class TextNode:
 
-    def __init__(self, text: str, text_type, url: str):        
+    def __init__(self, text: str, text_type: TextType, url: str =None):        
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -25,4 +29,22 @@ class TextNode:
         return True
     
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    
+def text_node_to_html_node(text_node: TextNode):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None,text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b",text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i",text_node.text)
+        case TextType.CODE:
+            return LeafNode("code",text_node.text)
+        case TextType.LINK:
+            return LeafNode("a",text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img","", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError(f"Invalid text type: {text_node.text_type}")
+
